97384d1dbd8f026b80e4a2983bc8ba92c40660c9
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-22 14:05:56 +0300

    Completed Lab

--- server/app.py
+++ server/app.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
-
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+# Component 1: Routing
+@app.route('/')
+def index():
+    # Component 3: Response Handling
+    return '''
+    <h1>Welcome to my app!</h1>
+    <p>Liverpool FC is the greatest club in the world.</p>
+    <p>Cristiano Ronaldo is the G.O.A.T</p>
+    '''
+
+# Component 1: Routing
+@app.route('/<string:username>')
+def user(username):
+    # Component 3: Response Handling
+    return f'<h1>Profile for {username}</h1>'
+
+if __name__ == '__main__':
+    # Component 7: Running the Development Server
+    app.run(port=5555, debug=True)
